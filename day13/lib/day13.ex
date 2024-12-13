0aaec50ac53b5b4da76d547df1b613c400164a2a
Nick Kijak <nkijak@gmail.com>
2024-12-13 16:37:17 -0500

    day13 parser

---
+++ day13/lib/day13.ex
@@ -0,0 +1,39 @@
+defmodule Day13 do
+  def parser(line, operator) do
+    [_, movement] = String.split(line, ": ")
+    {rawx,rawy} = String.split(movement, ", ") |> List.to_tuple
+    # TODO watch them add "-" in step 2
+    {_, x} = String.split(rawx, operator) |> List.to_tuple
+    {_, y} = String.split(rawy, operator) |> List.to_tuple
+    [x,y]
+      |> Enum.map(& Integer.parse(&1) |> elem(0))
+      |> List.to_tuple
+  end
+
+  def parse_machine(machine) do
+    [a,b,p] = machine
+    [coord_a, coord_b]  = [a,b] |> Enum.map(& parser(&1, "+"))
+
+    %{:a => coord_a, :b => coord_b, :prize => parser(p, "=")}
+  end
+
+  def parse(lines) do
+    chunkfn = fn el, acc ->
+      if el == "" do
+        {:cont, acc, []}
+      else
+        {:cont, acc ++ [el]}
+      end
+    end
+
+    afterfn = fn
+      [] -> {:cont, []}
+      acc -> {:cont, acc, []}
+    end
+
+    lines
+    |> Enum.chunk_while([],chunkfn, afterfn)
+    |> Enum.map(& Day13.parse_machine/1)
+  end
+
+end
---
+++ day13/test/day13_test.exs
@@ -0,0 +1,20 @@
+defmodule Day13Test do
+  use ExUnit.Case
+  doctest Day13
+
+  test "parse" do
+    lines = """
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+""" |> String.split("\n")
+    assert Day13.parse(lines) == [
+      %{:a => {94,34}, :b => {22,67}, :prize => {8400,5400}},
+      %{:a => {26,66}, :b => {67,21}, :prize => {12748,12176}},
+    ]
+  end
+end
